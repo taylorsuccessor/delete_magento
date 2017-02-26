e861c4b5505a197526fa96a9fa651949f7baddd4
taylorsuccessor <taylorsuccessor@gmail.com>
2017-02-26 07:12:35 -0800

    theme my theme

---
+++ app/design/frontend/Magento/myTheme/registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Magento/myTheme',
+    __DIR__
+);
+
+
+//\Magento\Framework\Component\ComponentRegistrar::register(
+//        \Magento\Framework\Component\ComponentRegistrar::THEME,
+//        'myTheme',
+//        __DIR__
+//    );
